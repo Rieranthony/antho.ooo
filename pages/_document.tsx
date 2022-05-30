ea376492edc7e9b4c06b4222e39e4d48cc299b25
Rieranthony <rieraanthony13@gmail.com>
2022-05-30 13:02:55 +0100

    feat: change favicon and metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,11 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500&display=swap"
             rel="stylesheet"
           />
+          <meta name="author" content="Anthony Riera" />
+          <meta
+            name="description"
+            content="Software engineer really interested in web3, great designs and good UX. I make youtube videos about how I approach software development, side project stories, and everything in between."
+          />
         </Head>
         <body>
           <Main />
